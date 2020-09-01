59dc38537453959e1064f0ca56930aeb9b6d6ba5
takanotume24 <takanotume24@gmail.com>
2020-09-01 19:59:14 +0900

    formated

--- pedsim_gazebo_plugin/scripts/pedsim_to_gazbo_world.py
+++ pedsim_gazebo_plugin/scripts/pedsim_to_gazbo_world.py
@@ -6,35 +6,39 @@ Created on Mon Dec  2 17:03:34 2019
 """
 import math
 from rospkg import RosPack
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
 
 global gzb_world
+
+
 def generate_pose(x1, y1, x2, y2):
-    if x2-x1==0:
-        print >> gzb_world, "\t  <pose frame=''>{} {}  1.4 0 0 {}</pose>".format( (x2+x1)/2, (y2+y1)/2, 1.57 )#x1, y1, math.atan((y2-y1)/(x2-x1)) )
+    if x2-x1 == 0:
+        print >> gzb_world, "\t  <pose frame=''>{} {}  1.4 0 0 {}</pose>".format(
+            (x2+x1)/2, (y2+y1)/2, 1.57)  # x1, y1, math.atan((y2-y1)/(x2-x1)) )
     else:
-        print >> gzb_world, "\t  <pose frame=''>{} {}  1.4 0 0 {}</pose>".format( (x2+x1)/2, (y2+y1)/2, math.atan( (y2-y1)/(x2-x1) )  )#x1, y1, math.atan((y2-y1)/(x2-x1)) )
+        print >> gzb_world, "\t  <pose frame=''>{} {}  1.4 0 0 {}</pose>".format(
+            (x2+x1)/2, (y2+y1)/2, math.atan((y2-y1)/(x2-x1)))  # x1, y1, math.atan((y2-y1)/(x2-x1)) )
+
 
 def generate_size(x1, y1, x2, y2):
-    l = math.sqrt( pow(y2-y1,2) + pow(x2-x1,2)  )
-    print >> gzb_world, "\t     <size> {} .2  2.8 </size>".format( l )
-    
-    
-    
+    l = math.sqrt(pow(y2-y1, 2) + pow(x2-x1, 2))
+    print >> gzb_world, "\t     <size> {} .2  2.8 </size>".format(l)
+
+
 def generate_obstacle(x1, y1, x2, y2, idx):
-     print >> gzb_world, '''
+    print >> gzb_world, '''
      <model name='grey_wall_{}'>
       <static>1</static>
       <link name='link'>
       '''.format(idx)
-     generate_pose(x1, y1, x2, y2)     
-     print >> gzb_world, '''    
+    generate_pose(x1, y1, x2, y2)
+    print >> gzb_world, '''    
         <collision name='collision'>
           <geometry>
             <box>
             '''
-     generate_size(x1, y1, x2, y2)
-     print >> gzb_world, '''
+    generate_size(x1, y1, x2, y2)
+    print >> gzb_world, '''
       \t   </box>
           </geometry>
           <max_contacts>10</max_contacts>
@@ -56,8 +60,8 @@ def generate_obstacle(x1, y1, x2, y2, idx):
           <geometry>
             <box>
             '''
-     generate_size(x1, y1, x2, y2)
-     print >> gzb_world, '''
+    generate_size(x1, y1, x2, y2)
+    print >> gzb_world, '''
       \t   </box>
        </geometry>
           <material>
@@ -73,34 +77,34 @@ def generate_obstacle(x1, y1, x2, y2, idx):
         <kinematic>0</kinematic>
       </link>
     </model>
-    '''.format( 1,1)#math.sqrt( pow(y2-y1,2) + pow(x2-x1,2)  ) )
+    '''.format(1, 1)  # math.sqrt( pow(y2-y1,2) + pow(x2-x1,2)  ) )
 
 
-
-def parseXML(xmlfile): 
-    tree = ET.parse(xmlfile) 
-    root = tree.getroot() 
+def parseXML(xmlfile):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
     idx = 0
     for item in root:
         if item.tag == 'obstacle':
-            idx= idx+1
-            x1= item.attrib['x1']
-            y1= item.attrib['y1']
-            x2= item.attrib['x2']
-            y2= item.attrib['y2']
-            generate_obstacle( float(x1), float(y1), float(x2), float(y2), idx)
+            idx = idx+1
+            x1 = item.attrib['x1']
+            y1 = item.attrib['y1']
+            x2 = item.attrib['x2']
+            y2 = item.attrib['y2']
+            generate_obstacle(float(x1), float(y1), float(x2), float(y2), idx)
 
-    
-def generate_gzb_world( pedsim_file_name ): 
+
+def generate_gzb_world(pedsim_file_name):
     rospack1 = RosPack()
     pkg_path = rospack1.get_path('pedsim_simulator')
-    xml_scenario =  pkg_path + "/scenarios/" + pedsim_file_name #bo_airport.xml"
+    xml_scenario = pkg_path + "/scenarios/" + pedsim_file_name  # bo_airport.xml"
     rospack2 = RosPack()
     pkg_path = rospack2.get_path('pedsim_gazebo_plugin')
-    gazebo_world =  pkg_path + "/worlds/" + pedsim_file_name.split('.')[0] + ".world" #bo_airport.xml"
-    global gzb_world    
+    gazebo_world = pkg_path + "/worlds/" + \
+        pedsim_file_name.split('.')[0] + ".world"  # bo_airport.xml"
+    global gzb_world
     with open(gazebo_world, 'w') as gzb_world:
-        print >> gzb_world, "<?xml version=\"1.0\" ?>"    
+        print >> gzb_world, "<?xml version=\"1.0\" ?>"
         print >> gzb_world, '''
     <!-- this file is auto generated using pedsim_gazebo_plugin pkg -->    
     <sdf version="1.5">
@@ -129,14 +133,14 @@ def generate_gzb_world( pedsim_file_name ):
     </sdf>
     
         '''
-    print "gazbo world has been generated: {}".format( gazebo_world)
-      
-      
-      
-def generate_launch_file( pedsim_file_name ): 
+    print "gazbo world has been generated: {}".format(gazebo_world)
+
+
+def generate_launch_file(pedsim_file_name):
     rospack2 = RosPack()
     pkg_path = rospack2.get_path('pedsim_gazebo_plugin')
-    launch_file =  pkg_path + "/launch/" + pedsim_file_name.split('.')[0] + ".launch" #bo_airport.xml"
+    launch_file = pkg_path + "/launch/" + \
+        pedsim_file_name.split('.')[0] + ".launch"  # bo_airport.xml"
     with open(launch_file, 'w') as launch:
         print >> launch, '''<launch>
 
@@ -153,16 +157,15 @@ def generate_launch_file( pedsim_file_name ):
 
 </launch>
 '''.format(pedsim_file_name.split('.')[0])
-    print "launch file has been generated: {}".format( launch_file )
- 
+    print "launch file has been generated: {}".format(launch_file)
 
-      
-if __name__ == "__main__": 
-    pedsim_file_name = raw_input(">> enter pedsim scenaria name: file_name.xml \n")
 
-    # genrate gazebo wolrd 
-    generate_gzb_world( pedsim_file_name )     
-    generate_launch_file( pedsim_file_name ) 
+if __name__ == "__main__":
+    pedsim_file_name = raw_input(
+        ">> enter pedsim scenaria name: file_name.xml \n")
+
+    # genrate gazebo wolrd
+    generate_gzb_world(pedsim_file_name)
+    generate_launch_file(pedsim_file_name)
     print ">> after you launch the scenario using pedsim_simulator, launch the generated world using: "
-    print " \" $roslaunch pedsim_gazebo_plugin {}.launch\"  ".format( pedsim_file_name.split('.')[0] )
-    
+    print " \" $roslaunch pedsim_gazebo_plugin {}.launch\"  ".format(pedsim_file_name.split('.')[0])
